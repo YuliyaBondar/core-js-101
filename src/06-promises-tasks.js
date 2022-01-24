c4afdd4d554a8e8ba2bba3508170ce19841cc44c
Yulia Bondar <yuliabondar98@gmail.com>
2022-01-25 01:07:40 +0300

    feat: add solutions to the third part of promises tasks

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -28,15 +28,16 @@
  *      .catch((error) => console.log(error.message)) // 'Error: Wrong parameter is passed!
  *                                                    //  Ask her again.';
  */
-function willYouMarryMe(/* isPositiveAnswer */) {
-  throw new Error('Not implemented');
-  // return new Promise((resolve, reject) => {
-  //   if (isPositiveAnswer) {
-  //     resolve('Hooray!!! She said "Yes"!');
-  //   } else {
-  //     reject('Oh no, she said "No".');
-  //   }
-  // });
+function willYouMarryMe(isPositiveAnswer) {
+  return new Promise((resolve, reject) => {
+    if (isPositiveAnswer === true) {
+      resolve('Hooray!!! She said "Yes"!');
+    } else if (isPositiveAnswer === false) {
+      resolve('Oh no, she said "No".');
+    } else {
+      reject(Error('Wrong parameter is passed! Ask her again.'));
+    }
+  });
 }
 
 
